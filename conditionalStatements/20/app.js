d01f364247dce6ea859866de734e661d77fc3e87
Viktor <loogo.viktor@mail.ru>
2022-01-11 01:50:27 +0300

    task update

--- conditionalStatements/20/app.js
+++ conditionalStatements/20/app.js
@@ -1,7 +1,14 @@
 let number1 = +prompt("Enter the first number");
 let number2 = +prompt("Enter the second number");
 let operation = prompt("Enter the operation");
+let flag = true;
 let result = (operation == "/") ? number1 / number2 : (operation == "+") ?
     number1 + number2 : (operation == "-") ? number1 - number2 : (operation == "*") ?
         number1 * number2 : "Incorrect operation";
-console.log(result);
+if (operation == "/" && number2 == 0) {
+    console.log("На 0 делить нельзя");
+    flag = false;
+}
+if (flag) {
+    console.log(result);
+}
